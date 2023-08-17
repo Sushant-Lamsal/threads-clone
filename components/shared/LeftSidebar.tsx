8756706b0644095bd9c701ec87652be2ba063087
Sushant Lamsal <sushantlamsal31@gmail.com>
2023-08-17 18:43:01 +0545

    feat : Logout button

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { sidebarLinks } from "@/constants";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import {usePathname,useRouter} from "next/navigation";
@@ -30,6 +31,26 @@ function LeftSidebar (){
                 >{link.label}</p>
                 </Link>
                 )})}
+            </div>
+            <div className="mt-10 px-6">
+            <SignedIn>
+                    <SignOutButton 
+                    signOutCallback={() =>
+                    router.push('/sign-in')
+                    }>
+                        <div className="flex cursor-pointer gap-4 p-4">
+                            <Image
+                            src="/assets/logout.svg"
+                            alt="logout"
+                            width={24}
+                            height={24}
+                            />
+                            <p className="text-light-2 max-lg:hidden">
+                            Logout
+                            </p>
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
             </div>
         </section>
     )
